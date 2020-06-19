72d989c8c354b69eefcfab69308a624999282b9f
abdulmoeid7112 <abdulmoeid7112@gmail.com>
2020-06-19 04:19:28 -0700

    Domain Model Added

---
+++ models/student.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"github.com/fatih/structs"
+)
+
+type Student struct {
+	Name  string `json:"name"`
+	Age   int32  `json:"age"`
+	Level string `json:"level"`
+	Phone string `json:"phone"`
+}
+
+func (s *Student) Map() map[string]interface{} {
+	return structs.Map(s)
+}
+
+func (s *Student) Names() []string {
+	return structs.Names(s)
+}
---
+++ models/student_test.go
@@ -0,0 +1,87 @@
+package models
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestStudent_Map(t *testing.T) {
+	type fields struct {
+		Name  string
+		Age   int32
+		Level string
+		Phone string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   map[string]interface{}
+	}{
+		{
+			name: "success",
+			fields: fields{
+				Name:  "Moeid",
+				Age:   21,
+				Level: "Bachelor",
+				Phone: "030051278936",
+			},
+			want: map[string]interface{}{
+				"Name":  "Moeid",
+				"Age":   21,
+				"Level": "Bachelor",
+				"Phone": "030051278936",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Student{
+				Name:  tt.fields.Name,
+				Age:   tt.fields.Age,
+				Level: tt.fields.Level,
+				Phone: tt.fields.Phone,
+			}
+			if got := s.Map(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Map() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestStudent_Names(t *testing.T) {
+	type fields struct {
+		Name  string
+		Age   int32
+		Level string
+		Phone string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		want   []string
+	}{
+		{
+			name: "success",
+			fields: fields{
+				Name:  "Moeid",
+				Age:   21,
+				Level: "Bachelor",
+				Phone: "030051278936",
+			},
+			want: []string{"Name", "Age", "Level", "Phone"},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &Student{
+				Name:  tt.fields.Name,
+				Age:   tt.fields.Age,
+				Level: tt.fields.Level,
+				Phone: tt.fields.Phone,
+			}
+			if got := s.Names(); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Names() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
